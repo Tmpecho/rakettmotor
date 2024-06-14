50c84581af8e109e3bd2f20750f1ee2ad0990f38
Johannes Aamot-Skeidsvoll <82368148+Tmpecho@users.noreply.github.com>
2024-06-14 14:54:25 +0200

    Sorter konstanter.py

--- config/konstanter.py
+++ config/konstanter.py
@@ -1,12 +1,12 @@
 class Konstanter:
-    k = 0.75  # luftmotstandskoeffisient
-    A = 0.005  # Rakettens tverrsnittsareal (m^2)
     RAKETT_MASSE = 0.08  # kg
     START_RAKETTMOTOR_MASSE = 0.02  # kg
     GAMMA = 6.67e-11  # m^3/kg/s^2
     JORDMASSE = 5.972e24  # kg
     JORDRADIUS = 6.371e6  # m
     # For luftmotstand/lufttetthet
+    k = 0.75  # luftmotstandskoeffisient
+    A = 0.005  # Rakettens tverrsnittsareal (m^2)
     P0 = 101_325  # Trykk ved havnivå (Pa)
     T0 = 288.15  # Temperatur havnivå (K)
     GAMMA_LR = 0.0065  # lapseraten (K/m)
